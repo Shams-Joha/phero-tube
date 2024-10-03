42b8670e82011007ac98d6071915701857073a1b
Shams-Joha <shamsjoha3@gmail.com>
2024-10-04 00:30:56 +0600

    Finished Navbar and started display category section

--- js/video.js
+++ js/video.js
@@ -1,1 +1,22 @@
 // Fetch, Load and Show Categories on html.
+// Create Load Categories
+
+// Create Load Categories
+const loadCategories = () => {
+    // Fetch the data.
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+
+}
+
+loadCategories();
+
+
+// create display Categories.
+const displayCategories = (data) => {
+    // add data in html
+    console.log(data);
+
+}
