ebb8248be5f47d262bea3a442daa89a83d0a8d97
Shams-Joha <shamsjoha3@gmail.com>
2024-10-04 12:51:29 +0600

    access object and display dynamic data to card

--- js/video.js
+++ js/video.js
@@ -36,24 +36,24 @@ const displayData = (categories) => {
 }
 
 const cardDemo = {
-    
-        category_id: "1001",
-        video_id: "aaaa",
-        thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
-        title: "Shape of You",
-        authors: [
-            {
-                profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
-                profile_name: "Olivia Mitchell",
-                verified: ""
-            }
-        ],
-        others: {
-            views: "100K",
-            posted_date: "16278"
-        },
-        description: "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-    
+
+    category_id: "1001",
+    video_id: "aaaa",
+    thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
+    title: "Shape of You",
+    authors: [
+        {
+            profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            profile_name: "Olivia Mitchell",
+            verified: ""
+        }
+    ],
+    others: {
+        views: "100K",
+        posted_date: "16278"
+    },
+    description: "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+
 };
 
 
@@ -66,16 +66,23 @@ const displayVideos = (videos) => {
         const card = document.createElement('div');
         card.classList = "card card-compact";
         card.innerHTML =
-            `<figure>
-    <img
+            `<figure class="h-[200px]">
+    <img class="h-full w-full object-cover"
       src=${item.thumbnail}/>
     </figure>
-    <div class="card-body">
-    <h2 class="card-title">${item.title}</h2>
-    <p>${item.description}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+    <div class="px-0 py-2 flex gap-2 items-start">
+    <div>
+    <img class="w-10 h-10 object-cover rounded-full" src="${item.authors[0].profile_picture}" />
     </div>
+    <div>
+     <h2 class="font-bold">${item.title}</h2>
+     <div class= "flex gap-2 items-center">
+     <p>${item.authors[0].profile_name}</p>
+     <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" /img>
+     </div>
+     
+     <p>${item.others.views} Views</p>
+     </div>
   </div>
         `
         videoContainer.append(card);
