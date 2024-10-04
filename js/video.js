968e7b0494d1fbc75138f7719d29da33822cef73
Shams-Joha <shamsjoha3@gmail.com>
2024-10-04 16:41:58 +0600

    finished empty category button condition

--- js/video.js
+++ js/video.js
@@ -59,8 +59,23 @@ const displayData = (categories) => {
 // display the Videos
 
 const displayVideos = (videos) => {
+    
     const videoContainer = document.getElementById('videos');
     videoContainer.innerHTML = "";
+
+    if(videos.length == 0){
+        videoContainer.classList.remove('grid');
+        videoContainer.innerHTML = `
+        <div class = "min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+        <img src="assets/icon.png">
+        <h2 class = "text-xl text-center">No Content here in this category</h2>
+        </div>
+        `;
+        return;
+    }
+    else{
+        videoContainer.classList.add('grid');
+    }
     videos.forEach((item) => {
         console.log(item);
         const card = document.createElement('div');
