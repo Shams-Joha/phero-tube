2855c8ba6d6d59f7768b05d6c0da2aa0b2eb4152
Shams-Joha <shamsjoha3@gmail.com>
2024-10-04 18:48:09 +0600

    implemented details modal

--- js/video.js
+++ js/video.js
@@ -8,6 +8,47 @@ const loadCategories = () => {
         .catch((error) => console.log(error));
 }
 
+
+//  {
+// "status": true,
+//     "message": "successfully fetched all the videos",
+//     "videos": [
+//       {
+//         "category_id": "1001",
+//         "video_id": "aaaa",
+//         "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//         "title": "Shape of You",
+//         "authors": [
+//           {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//           }
+//         ],
+//         "others": {
+//           "views": "100K",
+//           "posted_date": "16278"
+//         },
+//         "description": "Dive into the rhythm of 'Shape of You,' a 
+//         captivating track that blends pop sensibilities with vibrant beats.
+//          Created by Olivia Mitchell, this song has already gained 100K views since its release.
+//           With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for
+//            an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique
+//             style create a memorable listening journey."]]
+
+//       }
+
+// Load Details
+
+const loadDetails = async (videoID) => {
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoID}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayDetails(data.video);
+
+}
+
+
 // Load the videos
 const loadVideos = async () => {
     try {
@@ -52,6 +93,21 @@ const loadCategoryVideos = async (id) => {
 }
 
 // -------------------------------Display Functions----------------------------------
+
+// display Details
+
+const displayDetails = (video) => {
+    const modalContainer = document.getElementById('modal-content');
+    // way-1
+    // document.getElementById("showModalData").click();
+    // way-2
+    document.getElementById("customModal").showModal();
+    modalContainer.innerHTML = `
+    <img src=${video.thumbnail} />
+    <p>${video.description}</p>
+    `
+}
+
 // display the Categories
 const displayData = (categories) => {
     const categoryContainer = document.getElementById('categories');
@@ -114,6 +170,7 @@ const displayVideos = (videos) => {
      </div>
      
      <p>${item.others.views} Views</p>
+      <p> <button onclick="loadDetails('${item.video_id}')" class ="btn btn-sm btn-error">Details</button></p>
      </div>
   </div>
         `
