06c1c6c63fe0118231bc2e44af96637f8a4c360b
Shams-Joha <shamsjoha3@gmail.com>
2024-10-04 09:41:46 +0600

    finished adding categories using API

--- js/video.js
+++ js/video.js
@@ -5,18 +5,34 @@
 const loadCategories = () => {
     // Fetch the data.
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then((res) => res.json())
-    .then((data) => displayCategories(data.categories))
-    .catch((error) => console.log(error));
+        .then((res) => res.json())
+        .then((data) => displayCategories(data.categories))
+        .catch((error) => console.log(error));
 
 }
 
 loadCategories();
 
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
 
 // create display Categories.
-const displayCategories = (data) => {
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById('categories');
+
+
+
     // add data in html
-    console.log(data);
+    categories.forEach((item) => {
+        console.log(item);
+        //create a button
+        const button = document.createElement('button');
+        button.classList = "btn";
+        button.innerText = item.category;
+        categoryContainer.appendChild(button);
+
+    });
 
 }
